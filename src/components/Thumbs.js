657f99e54d095dcfd1f32762ccb4c6e10bbfe68f
shafinaz2006 <shafinaz2006@gmail.com>
2022-06-05 11:16:12 -0700

    add image onclick and clearInterval

---
+++ src/components/Thumbs.js
@@ -0,0 +1,47 @@
+import React, { Component, Fragment } from "react";
+import "./Thumbs.css";
+
+class Thumbs extends Component {
+  constructor(props) {
+    super(props);
+    this.onImageSelect = this.onImageSelect.bind(this);
+  }
+
+  onImageSelect(event, idx) {
+    this.props.selectedCatalog(idx);
+  }
+
+  render() {
+    return (
+      <Fragment>
+        {this.props.items.map((catalog, idx) => (
+          <span
+            className="catalog item"
+            onClick={(event) => this.onImageSelect(event, idx)}
+            // className={"thumb-select"}
+            id={idx}
+            key={idx}
+            data-testid={"thumb_outer_" + idx}
+          >
+            <span
+              className={
+                "thumb-outer " +
+                (idx == this.props.currentIndex ? "thumb-selected" : " ")
+              }
+              data-testid={"thumb_" + idx}
+            >
+              <span
+                className="thumb"
+                id={idx}
+                style={{ backgroundImage: "url(" + catalog.thumb + ")" }}
+                data-testid={"thumb_img_" + idx}
+              />
+            </span>
+          </span>
+        ))}
+      </Fragment>
+    );
+  }
+}
+
+export default Thumbs;
